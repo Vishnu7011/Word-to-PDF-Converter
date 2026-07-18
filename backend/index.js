d0b0392292b01a9cd4e421b43c4a0cb28f597405
Vishnu <vishnupal220@gmail.com>
2026-07-18 17:04:00 +0530

    Done !!!

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const multer  = require('multer')
+const cors = require('cors');
+const docxConverter = require('docx-pdf');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Setting up the file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/convertFile', upload.single('file'), (req, res, next)=> {
+try {
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: 'No file uploaded',
+      });
+    }
+
+    // DEFINING OUTPUT FILE PATH
+    const outoutPath = path.join(
+        __dirname, 
+        'files',
+        `${req.file.originalname}.pdf`
+    );
+  docxConverter(req.file.path, outoutPath, (err,result)=>{
+  if(err){
+    console.log(err);
+    return res.status(500).json({
+      message: 'Error converting docx to pdf',
+    })
+  }
+  res.download(outoutPath, (err) => {
+  console.log('file download');
+  })
+});
+} catch (error) {
+  console.log(error);
+  res.status(500).json({
+    message: 'Internal server error',
+  })
+}
+});
+
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+       <div className="item-center justify-center">
+       <hr className="border-black" />
+          <h1 className="text-center py-3 text-sm">
+          Copyright © 2024 - All right reserved bt ACME Industries Ltd</h1>
+       </div>
+    </>
+  )
+}
+
+export default Footer
